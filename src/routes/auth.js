1a7c5cd63b787acf9c1e5b565cfbe739fd654a68
Ava1ls <anslav05@gmail.com>
2020-12-11 22:20:46 +0200

    deleted prev auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const { register, login, getMe, /*forgotPassword, resetPassword,*/ updateDetails, updatePassword } = require('../controllers/auth')
+//const { register, login, getMe, /*forgotPassword, resetPassword,*/ updateDetails, updatePassword } = require('../controllers/auth')
 
 const router = express.Router()
 
-const {protect} = require('../middleware/authHandler');
   
-router.post('/register', register)
-router.post('/login', login)
-router.get('/me', protect, getMe)
+//router.post('/register', register)
+//router.post('/login', login)
+//router.get('/me', protect, getMe)
 //router.post('/forgotpassword', forgotPassword)
 //router.put('/resetpassword/:id', protect, resetPassword)
 //router.put('/updatedetails', updateDetails)
